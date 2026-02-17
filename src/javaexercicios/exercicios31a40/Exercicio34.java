7e030341f18a40240a01b0d862fbbfc03ef6846b
willa <willaevangelista.pro@gmail.com>
2026-02-17 14:41:08 -0300

    feat: add description for exercise 34

--- src/javaexercicios/exercicios31a40/Exercicio34.java
+++ src/javaexercicios/exercicios31a40/Exercicio34.java
@@ -1,3 +1,13 @@
+/*
+ * Warmup-1 > startHi
+ *
+ * Given a string, return true if the string starts with "hi" and false otherwise.
+ *
+ * - startHi("hi there") → true
+ * - startHi("hi") → true
+ * - startHi("hello hi") → false
+ */
+
 package javaexercicios.exercicios31a40;
 
 public class Exercicio34 {
